62bfeb79a4c08f904d953ae5f8ca525e10868f92
m3rofjdn-cyber <m3rofjdn@gmail.com>
2026-09-01 21:45:03 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const { OAuth2Client } = require('google-auth-library');
 const app = express();
 app.use(express.json());
 app.use(cors());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname));
 
 const PORT = process.env.PORT || 3000;
 const JWT_SECRET = process.env.JWT_SECRET;
